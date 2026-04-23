29438403ffc0815e509a132ad0787fab00521c37
Adam Gillion <adam@queuedesk.org>
2026-04-23 22:32:22 +0100

    Add files via upload

---
+++ fizzbuzz/fizzbuzz/fizzbuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            for (i = 1; i < 101; i++)
+            {
+                if (((i % 3) == 0) && ((i % 5) == 0))
+                {
+                    Console.WriteLine("FizzBuzz");
+
+                }
+                else if ((i % 3) == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if ((i % 5) == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Press enter to complete");
+            Console.ReadLine();
+        }
+    }
+}
