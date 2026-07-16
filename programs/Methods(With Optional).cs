8fe30dfdf5b7316f74dd50cab4713a18f6c16ff1
Adam Gillion <adam@queuedesk.org>
2026-07-16 17:51:40 +0100

    Create Methods(With Optional).cs

---
+++ programs/Methods(With Optional).cs
@@ -0,0 +1,49 @@
+// for context see: https://learn.microsoft.com/en-gb/training/modules/create-c-sharp-methods-parameters/3-exercise-understand-method-scope
+double pi = 3.14159;
+
+void PrintCircleArea(int radius)
+{
+    double area = pi * (radius * radius);
+    Console.WriteLine($"Area = {area}");
+}
+
+void PrintCircleCircumference(int radius)
+{
+    double circumference = 2 * pi * radius;
+    Console.WriteLine($"Circumference = {circumference}");
+}
+
+// email challenge
+
+string[,] corporate = // string[,] defines a multi-dimensional array
+	{
+		{"Robert", "Bavin"}, {"Simon", "Bright"},
+		{"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+		{"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+    };
+
+	string[,] external = 
+	{
+		{"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+		{"Shay", "Lawrence"}, {"Daren", "Valdes"}
+	};
+
+	string externalDomain = "hayworth.com";
+
+	for (int i = 0; i < corporate.GetLength(0); i++) 
+	{
+		DisplayEmail(first: corporate[i,0], last: corporate[i,1]);
+	}
+
+	for (int i = 0; i < external.GetLength(0); i++) 
+	{
+		DisplayEmail(first: external[i,0], last: external[i,1], domain: externalDomain);
+	}
+
+	void DisplayEmail(string first, string last, string domain = "contoso.com") 
+    //by defining the domain, it is an optional element as if it isnt passed it defaults to contoso.com
+	{
+		string email = first.Substring(0, 2) + last;
+		email = email.ToLower();
+		Console.WriteLine($"{email}@{domain}");
+	}
